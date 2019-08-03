aa2499417ec0f1e6e3a85b90dec6788ca5de31ec
Marcus Runge <marcus.runge@outlook.com>
2019-08-03 16:42:06 +0200

    readfile

--- WifiTcpServer/WifiTcpServer.ino
+++ WifiTcpServer/WifiTcpServer.ino
@@ -3,19 +3,60 @@
 #include <LWiFiClient.h>
 #include "rgb_lcd.h"
 #include <LBattery.h>
+#include <LBT.h> 
+#include <LBTServer.h>
+#include <LStorage.h>
+#include <LFlash.h>
+
 #define WIFI_AP "Toronto Pearson Wi-Fi"
 #define WIFI_PASSWORD "$unWing#2012" 
 #define WIFI_AUTH LWIFI_WPA  // choose from LWIFI_OPEN, LWIFI_WPA, or LWIFI_WEP according to your WiFi AP configuration
+#define Drv LFlash
+
+/*struct WifiConfig {
+  char wifi_ap[32];
+  char wifi_password[32];
+  int wifi_auth;
+};*/
+
+void printWifiStatus();
+void bridgeConnection();
+void bluetoothConnection();
 
 LWiFiServer server(5401);
 rgb_lcd lcd;
+String config;
+LFile configFile;
+/*WifiConfig wifiConfig;*/
+
 void setup()
 {
+  Drv.begin();
+  LBTServer.begin((uint8_t*)"WiFiTcpServer");
   LWiFi.begin();
   Serial1.begin(9600);
   Serial.begin(9600);
   lcd.begin(16, 2);
   lcd.setRGB(255, 0, 0);
+  if (Drv.exists("wifi.txt")) 
+    {
+      configFile = Drv.open("wifi.txt");
+      if(configFile)
+      {
+        configFile.seek(0); 
+        char wifiConfigJson[configFile.size()+1];
+        int i =0;               
+        while (configFile.available()) 
+        {  
+          wifiConfigJson[i]=configFile.read();
+          i++;
+        }
+        wifiConfigJson[i]= 0;
+        Serial.println(wifiConfigJson);
+        Serial.println(i);
+        Serial.println(configFile.size());
+      } 
+    }
   // keep retrying until connected to AP
   Serial.println("Connecting to AP");
   while (0 == LWiFi.connect(WIFI_AP, LWiFiLoginInfo(WIFI_AUTH, WIFI_PASSWORD)))
@@ -31,22 +72,8 @@ void setup()
 
 void loop()
 {
-  delay(500);
-  LWiFiClient client = server.available();
-  if (client)
-  {    
-    while (client.connected())
-    {
-      while (client.available())
-      {
-        Serial1.write(client.read());                        
-      }
-      while (Serial1.available()) 
-      {
-        client.write(Serial1.read());
-      }      
-    }
-  }
+  bridgeConnection();
+  bluetoothConnection();
 }
 
 void printWifiStatus()
@@ -79,4 +106,50 @@ void printWifiStatus()
   Serial.print("Battery Level: ");
   Serial.print(LBattery.level());
   Serial.println("%");;
+}
+
+void bridgeConnection()
+{
+delay(500);
+  LWiFiClient client = server.available();
+  if (client)
+  {    
+    while (client.connected())
+    {
+      while (client.available())
+      {
+        Serial1.write(client.read());                        
+      }
+      while (Serial1.available()) 
+      {
+        client.write(Serial1.read());
+      }      
+    }
+  }
+}
+
+void bluetoothConnection()
+{
+  if(LBTServer.connected())
+  {
+    config = LBTServer.readString();
+    if (Drv.exists("wifi.txt")) 
+    {
+      Drv.remove("wifi.txt"); 
+    }
+    else 
+    {
+      configFile = Drv.open("wifi.txt", FILE_WRITE);
+      if (configFile) 
+      {
+        configFile.println(config);
+        configFile.close();
+      }      
+    }    
+    LBTServer.write("acknowledged");
+  }
+  else
+  {
+    LBTServer.accept(5);
+  }  
 }
